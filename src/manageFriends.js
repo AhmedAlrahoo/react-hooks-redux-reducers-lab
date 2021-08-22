6c494975f09d05f6cd338b0879b8e60689419cdc
ahmed <a.alrahoo2001@gmail.com>
2021-08-22 22:33:19 +0300

    Done.

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,25 @@
+const state = {friends : []};
 export function manageFriends(state, action) {
-  // your code here
+  //your code here
+  
+  switch (action.type){
+    case "friends/add" :
+      return {friends : [...state.friends,action.payload]};
+      case "friends/remove" :
+        let updatedFriends = state.friends.filter((item)=> item.id != action.payload)
+        return {friends : updatedFriends}
+        default : return state
+  }
 }
+manageFriends(state,{
+  type: "friends/add",
+  payload: {
+    name: "Chrome Boi",
+    homewtown: "NYC",
+    id: 1
+  }
+} )
+manageFriends(state,{
+  type: "friends/remove",
+  payload: 1,
+} )
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,13 @@
 export function managePresents(state, action) {
-  // your code here
+  // your code heer
+  state = {numberOfPresents : 0}
+  switch (action.type){
+    case "presents/increase" :
+      return {numberOfPresents : state.numberOfPresents + 1};
+      default : return state;
+  }
 }
+
+managePresents({numberOfPresents:0},{
+  type: "presents/increase",
+} )
